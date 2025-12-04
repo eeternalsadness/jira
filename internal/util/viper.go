1c580049577963cd69986cc23730b603fe98b55f
Bach Nguyen <69bnguyen@gmail.com>
2025-12-04 15:48:12 +0800

    update ViperUpsert functions to take in a pointer for default value & handle sensitive string config

--- internal/util/viper.go
+++ internal/util/viper.go
@@ -10,11 +10,22 @@ import (
 	"github.com/spf13/viper"
 )
 
-func ViperUpsertString(key string, prompt string, example string) error {
+func ViperUpsertString(key string, prompt string, defaultVal *string, sensitive bool) error {
 	reader := bufio.NewReader(os.Stdin)
 
-	if len(example) > 0 {
-		fmt.Printf("%s [%s]: ", prompt, example)
+	currentVal := viper.GetString(key)
+
+	// if there is an existing value, use it instead of the default val
+	if viper.IsSet(key) {
+		defaultVal = &currentVal
+	}
+
+	if defaultVal != nil {
+		if sensitive {
+			fmt.Printf("%s [%s]: ", prompt, sensorString(*defaultVal))
+		} else {
+			fmt.Printf("%s [%s]: ", prompt, *defaultVal)
+		}
 	} else {
 		fmt.Printf("%s: ", prompt)
 	}
@@ -22,28 +33,25 @@ func ViperUpsertString(key string, prompt string, example string) error {
 	userInput, _ := reader.ReadString('\n')
 	userInput = userInput[:len(userInput)-1]
 
-	// check for existing config
-	if viper.GetString(key) != "" {
-		overwrite, err := UserYesNo(fmt.Sprintf("Configuration for key '%s' already exists. Overwrite?", key))
-		if err != nil {
-			return err
-		}
-
-		if overwrite {
-			viper.Set(key, userInput)
-		}
-	} else {
+	if len(userInput) > 0 {
 		viper.Set(key, userInput)
+	} else if defaultVal != nil {
+		viper.Set(key, *defaultVal)
 	}
 
 	return nil
 }
 
-func ViperUpsertInt(key string, prompt string, example string) error {
+func ViperUpsertInt(key string, prompt string, defaultVal *int) error {
 	reader := bufio.NewReader(os.Stdin)
 
-	if len(example) > 0 {
-		fmt.Printf("%s [%s]: ", prompt, example)
+	// if there is an existing value, use it instead of the default val
+	if viper.IsSet(key) {
+		*defaultVal = viper.GetInt(key)
+	}
+
+	if defaultVal != nil {
+		fmt.Printf("%s [%d]: ", prompt, *defaultVal)
 	} else {
 		fmt.Printf("%s: ", prompt)
 	}
@@ -51,25 +59,30 @@ func ViperUpsertInt(key string, prompt string, example string) error {
 	userInput, _ := reader.ReadString('\n')
 	userInput = userInput[:len(userInput)-1]
 
-	// check if input string is valid int
-	userInputInt, err := strconv.Atoi(strings.TrimSpace(userInput))
-	if err != nil {
-		return err
-	}
-
-	// check for existing config
-	if viper.GetString(key) != "" {
-		overwrite, err := UserYesNo(fmt.Sprintf("Configuration for key '%s' already exists. Overwrite?", key))
+	if len(userInput) > 0 {
+		// check if input string is valid int
+		userInputInt, err := strconv.Atoi(strings.TrimSpace(userInput))
 		if err != nil {
 			return err
 		}
-
-		if overwrite {
-			viper.Set(key, userInputInt)
-		}
-	} else {
 		viper.Set(key, userInputInt)
+	} else {
+		if defaultVal == nil {
+			return fmt.Errorf("invalid number")
+		}
+		viper.Set(key, *defaultVal)
 	}
 
 	return nil
+}
+
+func sensorString(str string) string {
+	// otherwise show the first and last 25% chars (max 4)
+	charsToShow := len(str) / 4
+	charsToShow = min(charsToShow, 4)
+
+	// limit the sensored part to 20 chars
+	sensoredLen := min(len(str)-charsToShow*2, 20)
+
+	return fmt.Sprintf("%s%s%s", str[:charsToShow], strings.Repeat("*", sensoredLen), str[len(str)-charsToShow:])
 }
